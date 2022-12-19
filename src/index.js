5dfb3a17b26d05f582ef4530f5c9dffaa605cc1f
obed300 <obamfo@st.ug.edu.gh>
2022-12-19 08:32:38 +0000

    Add content to files

--- src/index.js
+++ src/index.js
@@ -1,26 +1,58 @@
 import './style.css';
 
-const tasks = [
-  {
-    description: 'sfdgdgs',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'hfthyh',
-    completed: false,
-    index: 1,
-  },
-];
+class UI {
+  static displaystructure() {
+    const tasks = [
+      {
+        description: 'sfdgdgs',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'hfthyh',
+        completed: false,
+        index: 1,
+      },
+    ];
+    tasks.forEach((element) => UI.addlist(element));
+  }
 
-const listcontainer = document.querySelector('.list-container');
-tasks.forEach((element) => {
-  const list = document.createElement('div');
-  list.classList.add('mylist');
-  list.innerHTML = `
+  static addlist(element) {
+    const listcontainer = document.querySelector('.list-container');
+
+    const list = document.createElement('div');
+    list.classList.add('mylist');
+    list.innerHTML = `
     <i class="fa fa-square-o"></i>
     <div class="describe">${element.description}</div>
     <i class="uil uil-ellipsis-v"></i>
     `;
-  listcontainer.appendChild(list);
-});
+    listcontainer.appendChild(list);
+  }
+}
+document.addEventListener('DOMContentLoaded', UI.displaystructure);
+
+// const tasks = [
+//   {
+//     description: 'sfdgdgs',
+//     completed: false,
+//     index: 1,
+//   },
+//   {
+//     description: 'hfthyh',
+//     completed: false,
+//     index: 1,
+//   },
+// ];
+
+// const listcontainer = document.querySelector('.list-container');
+// tasks.forEach((element) => {
+//   const list = document.createElement('div');
+//   list.classList.add('mylist');
+//   list.innerHTML = `
+//     <i class="fa fa-square-o"></i>
+//     <div class="describe">${element.description}</div>
+//     <i class="uil uil-ellipsis-v"></i>
+//     `;
+//   listcontainer.appendChild(list);
+// });
